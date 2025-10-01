139a9181c4ec58ca36a5e58f5ede01c76043ce14
Bati4eli <mister.happyend@gmail.com>
2025-10-01 09:24:27 +0300

    Added Google Map!

--- app/src/main/java/ru/bati4eli/smartcloud/android/client/MyApplication.java
+++ app/src/main/java/ru/bati4eli/smartcloud/android/client/MyApplication.java
@@ -1,5 +1,6 @@
 package ru.bati4eli.smartcloud.android.client;
 
+
 import android.app.Application;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.EntypoModule;
@@ -11,12 +12,15 @@ import com.joanzapata.iconify.fonts.MeteoconsModule;
 import com.joanzapata.iconify.fonts.SimpleLineIconsModule;
 import com.joanzapata.iconify.fonts.TypiconsModule;
 import com.joanzapata.iconify.fonts.WeathericonsModule;
+import lombok.val;
 
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        logUncaughtException();
+
         Iconify
 //                .with(new MyFontAwesomeModule())
                 .with(new FontAwesomeModule())
@@ -29,5 +33,27 @@ public class MyApplication extends Application {
                 .with(new SimpleLineIconsModule())
                 .with(new IoniconsModule())
         ;
+    }
+
+
+    /**
+     * Логирование внезапных исключений.
+     */
+    private void logUncaughtException() {
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler((Thread t, Throwable e) -> uncaughtException(defaultHandler, t, e));
+    }
+
+    public void uncaughtException(Thread.UncaughtExceptionHandler defaultHandler, Thread t, Throwable e) {
+        // Логируем краткое описание
+        android.util.Log.e("MyCloud", "Uncaught exception in thread: " + t.getName() + " (" + t.getId() + ")", e);
+
+        // Передаём дальше системному обработчику, чтобы процесс корректно завершился
+        if (defaultHandler != null) {
+            defaultHandler.uncaughtException(t, e);
+        } else {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(10);
+        }
     }
 }
