6b1acc17418bdc40a82d1a1f6e93affee6ccca32
Bati4eli <mister.happyend@gmail.com>
2025-10-14 11:09:42 +0300

    UI improvements for PhotosFragment and album cards

--- app/src/main/java/ru/bati4eli/smartcloud/android/client/tabs/PhotosFragment.java
+++ app/src/main/java/ru/bati4eli/smartcloud/android/client/tabs/PhotosFragment.java
@@ -34,12 +34,9 @@ import ru.bati4eli.smartcloud.android.client.utils.MyUtils;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
